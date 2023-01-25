77487f9ffe690b12f395b4dece88b0286bcfc9cf
ekankr2 <ekankr2@naver.com>
2023-01-25 20:18:45 +0900

    init: initialize a spring project

---
+++ spring_backend/src/main/kotlin/com/example/spring_backend/SpringBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBackendApplication>(*args)
+}
