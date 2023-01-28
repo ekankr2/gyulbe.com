ebcf60f36c0de802552cffcd1f64d05bfb009776
ekank <ekankr2@naver.com>
2023-01-29 05:20:30 +0900

    feat: create pageable getPosts API

--- ekan_blog/src/main/kotlin/com/gyulbe/ekan_blog/controller/PostController.kt
+++ ekan_blog/src/main/kotlin/com/gyulbe/ekan_blog/controller/PostController.kt
@@ -3,6 +3,9 @@ package com.gyulbe.ekan_blog.controller
 import com.gyulbe.ekan_blog.domain.post.Post
 import com.gyulbe.ekan_blog.requests.post.PostCreateRequest
 import com.gyulbe.ekan_blog.service.post.PostService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/post")
+@RequestMapping("/posts")
 class PostController(
     private val postService: PostService
 ) {
@@ -21,7 +24,7 @@ class PostController(
     }
 
     @GetMapping("")
-    fun getPost(): String {
-        return "ok"
+    fun getAllPosts(@PageableDefault(page = 0, size = 20, sort = ["id"]) pageable: Pageable): Page<Post> {
+        return postService.getPosts(pageable)
     }
 }
