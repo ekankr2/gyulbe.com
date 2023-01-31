0ac9998d8fe8022d4abb9c3424e2c8a39283fde0
ekankr2 <ekankr2@naver.com>
2023-01-31 19:52:02 +0900

    chore(package): install Zustand, react-query [BLOG-SERVER-7]

--- blog-ui/src/main.tsx
+++ blog-ui/src/main.tsx
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.scss';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 30000,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </QueryClientProvider>,
 );
