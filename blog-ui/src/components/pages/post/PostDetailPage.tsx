6549093d5b9ec63a9dd43de667b70b8b04a10297
ekank <ekankr2@naver.com>
2023-02-05 01:28:51 +0900

    fix: post detail page JSX syntax error

--- blog-ui/src/components/pages/post/PostDetailPage.tsx
+++ blog-ui/src/components/pages/post/PostDetailPage.tsx
@@ -7,20 +7,22 @@ const PostDetailPage = () => {
   const { data: postInfo } = useGetPostInfo(id);
 
   return (
-    postInfo && (
-      <section className="mx-[20px] mt-[40px] md:mx-auto md:max-w-[750px]">
-        <article>
-          <div className="w-full">
-            <img className="h-full max-h-[300px] w-full rounded-xl" src="/sample-img.jpg" alt="main-image" />
-            <h1 className="mt-[20px] text-5xl font-bold">{postInfo.title}</h1>
-            <p className="mt-[20px] text-sm text-zinc-400">{new Date(postInfo.createdAt).toLocaleDateString()}</p>
-          </div>
-        </article>
-        <article className="mt-[50px]">
-          <p className="text-gray-600">{postInfo.content}</p>
-        </article>
-      </section>
-    )
+    <>
+      {postInfo &&
+        <section className="mx-[20px] mt-[40px] md:mx-auto md:max-w-[750px]">
+          <article>
+            <div className="w-full">
+              <img className="h-full max-h-[300px] w-full rounded-xl" src="/sample-img.jpg" alt="main-image" />
+              <h1 className="mt-[20px] text-5xl font-bold">{postInfo.title}</h1>
+              <p className="mt-[20px] text-sm text-zinc-400">{new Date(postInfo.createdAt).toLocaleDateString()}</p>
+            </div>
+          </article>
+          <article className="mt-[50px]">
+            <p className="text-gray-600">{postInfo.content}</p>
+          </article>
+        </section>
+      }
+    </>
   );
 };
 
