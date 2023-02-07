ddc9e50b07f847d98eac1ca8c6a1d624aa05eaeb
ekankr2 <ekankr2@naver.com>
2023-02-07 20:00:27 +0900

    feat: add three dots svg icon for update, delete[BLOG-15]

--- blog-ui/src/components/pages/post/PostDetailPage.tsx
+++ blog-ui/src/components/pages/post/PostDetailPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useGetPostInfo } from '../../../api/hooks/postHooks';
+// @ts-ignore
+import { ReactComponent as VerticalDots } from '../../../assets/dots-vertical.svg';
 
 const PostDetailPage = () => {
   const { id } = useParams<{ id: string }>();
@@ -8,20 +10,25 @@ const PostDetailPage = () => {
 
   return (
     <>
-      {postInfo &&
+      {postInfo && (
         <section className="mx-[20px] mt-[40px] md:mx-auto md:max-w-[750px]">
-          <article>
-            <div className="w-full">
-              <img className="h-full max-h-[300px] w-full rounded-xl" src="/sample-img.jpg" alt="main-image" />
-              <h1 className="mt-[20px] text-5xl font-bold">{postInfo.title}</h1>
-              <p className="mt-[20px] text-sm text-zinc-400">{new Date(postInfo.createdAt).toLocaleDateString()}</p>
+          <article className="border-b-[1px] border-solid border-gray-200 pb-[20px]">
+            <img className="h-full max-h-[300px] w-full rounded-xl" src="/sample-img.jpg" alt="main-image" />
+            <div className="flex w-full items-end justify-between">
+              <div>
+                <h1 className="mt-[20px] text-5xl font-bold">{postInfo.title}</h1>
+                <p className="mt-[20px] text-sm text-zinc-400">{new Date(postInfo.createdAt).toLocaleDateString()}</p>
+              </div>
+              <div className="cursor-pointer">
+                <VerticalDots width={24} height={24} fill="#a1a1aa" />
+              </div>
             </div>
           </article>
-          <article className="mt-[50px]">
+          <article className="pt-[40px]">
             <p className="text-gray-600">{postInfo.content}</p>
           </article>
         </section>
-      }
+      )}
     </>
   );
 };
