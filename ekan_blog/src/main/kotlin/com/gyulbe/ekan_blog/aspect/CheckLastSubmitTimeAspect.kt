50e297390d7c0161b2263fb5a75f9c744964bcb9
ekank <ekankr2@naver.com>
2023-02-19 16:29:18 +0900

    feat: make collection value concurrentHashMap

--- ekan_blog/src/main/kotlin/com/gyulbe/ekan_blog/aspect/CheckLastSubmitTimeAspect.kt
+++ ekan_blog/src/main/kotlin/com/gyulbe/ekan_blog/aspect/CheckLastSubmitTimeAspect.kt
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.Aspect
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletRequestAttributes
+import java.util.concurrent.ConcurrentHashMap
 
 
 @Aspect
@@ -15,14 +16,17 @@ class CheckLastSubmitTimeAspect {
     @Throws(Throwable::class)
     @Around("@annotation(com.gyulbe.ekan_blog.annotation.CheckLastSubmitTime)")
     fun checkLastSubmitTime(joinPoint: ProceedingJoinPoint): Any? {
+        val recentRequests: ConcurrentHashMap<String, Any> = ConcurrentHashMap()
         val request = (RequestContextHolder.currentRequestAttributes() as ServletRequestAttributes).request
-        val url = request.requestURL
-        val method = request.method
+        val clientIp = request.remoteAddr
+        val requestOptions = mapOf("requestUrl" to request.requestURL, "method" to request.method)
 
-        println(url)
-        println(method)
+        recentRequests[clientIp] = requestOptions
+
+        println(recentRequests[clientIp])
+
         val proceed = joinPoint.proceed()
-        println("after start")
+        println("after call")
         return proceed
     }
 }
